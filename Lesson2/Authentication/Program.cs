2f3b8aea07edd1660887516f1c336be31ec04a69
okuzmina <45969518+okuzmina@users.noreply.github.com>
2019-03-06 11:21:06 +0300

    new branch for Lesson2

---
+++ Lesson2/Authentication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Authentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "User1";
+            string password = "qwe123";
+            int counter = 3;
+
+            while (counter > 0)
+            {
+                Console.Write("\rEnter login: ");
+                string loginInput = Console.ReadLine();
+
+                Console.Write("\rEnter password: ");
+                string passwordInput = Console.ReadLine();
+
+                if (login == loginInput && password == passwordInput)
+                {
+                    Console.WriteLine("\nYou have been successfully logged in");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\rEntered login or password is invalid");
+                    Console.WriteLine("\rAttempts remaining: {0}", --counter);
+                    Console.WriteLine();
+                    if (counter == 0)
+                        Console.WriteLine("\rError: there are no more attempts left");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson2/Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static double Check()
+        {
+            int checkedInput = 0;
+            string input = Console.ReadLine();
+
+            while (!Int32.TryParse(input, out checkedInput))
+            {
+                Console.Write("Integer required! Try again: ");
+                input = Console.ReadLine();
+            }
+
+            return checkedInput;
+        }
+        static void Main(string[] args)
+        {
+            double operand1 = 0, operand2 = 0;
+
+            Console.Write("Enter first operand: ");
+            operand1 = Check();
+
+            Console.Write("\r\nEnter second operand: ");
+            operand2 = Check();
+
+            Console.Write("\nEnter operation: ");
+            char operation = Console.ReadKey().KeyChar;
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine("\n{0} + {1} = {2}", operand1, operand2, operand1 + operand2);
+                    break;
+                case '-':
+                    Console.WriteLine("\n{0} - {1} = {2}", operand1, operand2, operand1 - operand2);
+                    break;
+                case '/':
+                    Console.WriteLine("\n{0} / {1} = {2}", operand1, operand2, operand1 / operand2);
+                    break;
+                case '*':
+                    Console.WriteLine("\n{0} * {1} = {2}", operand1, operand2, operand1 * operand2);
+                    break;
+                case '^':
+                    Console.WriteLine("\n{0} ^ {1} = {2}", operand1, operand2, Math.Pow(operand1, operand2));
+                    break;
+                default:
+                    Console.WriteLine("\nEntered operation doesn't exist.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
